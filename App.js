38125a2bb9d2fdeca6d774b2489fd3493cf8503a
Khanh Hoang <hoangkhanh297@gmail.com>
2020-11-06 23:12:38 +0700

    Update package

--- App.js
+++ App.js
@@ -1,114 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { Text, View, StyleSheet, StatusBar, Navigator } from 'react-native';
+import Authentication from './src/components/authentication/Authentication.js'
+import ChangeInfo from './src/components/changeInfo/ChangeInfo.js'
+import Main from './src/components/main/Main.js'
+import OrderHistory from './src/components/orderHistory/OrderHistory.js'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+StatusBar.setHidden(true);
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <Navigator
+      initialRoute={{
+        name: 'MAIN'
+      }}
+      renderScene={(route, navigator) => {
+        switch (route.name) {
+          case 'MAIN':
+            return <Main></Main>;
+          case 'CHANGE_INFO':
+            return <ChangeInfo></ChangeInfo>;
+          case 'AUTHENTICATION':
+            return <Authentication></Authentication>;
+          default:
+            return <OrderHistory></OrderHistory>
+        }
+      }}
+    >
+      <View style={styles.view}>
+        <View style={styles.text}>
+          <Text>Welcome to my shop</Text>
+        </View>
+      </View>
+    </Navigator>
+
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  view: { flex: 1, backgroundColor: "white", alignItems: "center", justifyContent: "center" },
+  text: {
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "red"
+  }
 });
-
 export default App;
